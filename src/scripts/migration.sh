ecdaa85b6d0b37adb98d7cde29d0f11f53dd5f82
matentzn <nicolas.matentzoglu@gmail.com>
2019-11-13 08:10:43 +0000

    migration script 1

--- src/scripts/migration.sh
+++ src/scripts/migration.sh
@@ -7,8 +7,16 @@ git fetch
 git checkout origin/master -- fypo_edit.obo
 cp fypo_edit.obo src/ontology/fypo-edit.obo
 
-cd ~/ws/fypo/src/scripts
+cd ~/ws/fypo/src/ontology
 sh run.sh robot filter -i ../ontology/fypo-edit.obo --axioms "EquivalentClasses" --preserve-structure false -o ../ontology/components/fypo-eqs.ofn
 mv ../ontology/components/fypo-eqs.ofn ../ontology/components/fypo-eqs.owl
-sed -i '/^intersection_of[:]/d' src/ontology/fypo-edit.obo
+sed -i '' '/^intersection_of[:]/d' fypo-edit.obo
+sed -i '' '/^[<]/d' fypo-edit.obo
+sed -i '' '/^xref[:][ ]BFO[:]0000062/d' fypo-edit.obo
+sed -i '' '/^xref[:][ ]BFO[:]0000063/d' fypo-edit.obo
+sed -i '' '/^xref[:][ ]RO[:]0002353/d' fypo-edit.obo
+sed -i '' '/^xref[:][ ]RO[:]0002234/d' fypo-edit.obo
+
+echo "WARN: Add xref: RO:0002573 to qualifier typedef!"
+
 
