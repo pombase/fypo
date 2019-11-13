a5316cff8e9562307a2ff24f84c41014d769b660
matentzn <nicolas.matentzoglu@gmail.com>
2019-11-13 07:29:56 +0000

    odk updates

---
+++ scripts/migration.sh
@@ -0,0 +1,13 @@
+# checkout fypo-edit.obo from master
+
+set -e
+
+cd ~/ws/fypo
+git fetch
+git checkout origin/master -- src/ontology/fypo-edit.obo
+
+cd ~/ws/fypo/src/scripts
+sh run.sh robot filter -i ../ontology/fypo-edit.obo --axioms "EquivalentClasses" --preserve-structure false -o ../ontology/components/fypo-eqs.ofn
+mv ../ontology/components/fypo-eqs.ofn ../ontology/components/fypo-eqs.owl
+sed -i '/^intersection_of[:]/d' src/ontology/fypo-edit.obo
+
